7ba911774258d43ec31d852a138818e2c8a2987f
Angika Singh <angikasingh@Angikas-MacBook-Air.local>
2020-04-05 15:37:29 -0700

    Fixing GetInTouch Page and Navigation

--- src/GetInTouch/GetInTouch.jsx
+++ src/GetInTouch/GetInTouch.jsx
@@ -1,81 +1,61 @@
 import React, { Component } from "react";
 import styles from "./GetInTouch.css";
+import { render } from "react-dom";
 
 class GetInTouchForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            
-        };
-        
+        this.state = {value: ''};
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleShare = this.handleShare.bind(this);
-        }
-        handleChange(event) {
-            this.setState({value: event.target.value});
-        }
-
-        handleSubmit(event) {
-            alert('Thank you '+ this.state.value +' your feedback is submitted ');
-            event.preventDefault();
-        }
-
-        handleShare(event) {
-            alert('Thank you '+this.state.value+' for sharing your recipe with us. We will review and feature it on Foodwali ');
-            event.preventDefault();
-        }
-
-        render() {
-            return (
-                <html> 
-                <head> 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-                </head>
-                <form onSubmit = {this.handleSubmit} >
-                <div id="divPart">  
-                    <ul id="GetInTouchList"> 
-                    <li > 
-                    {/* <label className = {styles.name}> Name </label>
-                    <input type = "text" onChange = {this.handleChange} id = "box"/> */}
-                    <input class="input-effect" type = "text" name = "fullname"></input><br/>
-                        <label> Your name </label>
-                        <span class = "focus-border"></span>
-                        <br/>
-
-                    <input class="input-effect" type = "text" Email = "fullname"></input><br/>
-                        <label> Your email</label>
-                        <span class = "focus-border"></span><br/><br/>
-
-                        <label> 
-                            <input type = "radio" name ="options" value = "Drop us a recipe" checked = {true} className = "form-check-input" />
-                            Drop us a recipe
-                        </label>
-                        <label>
-                        <textarea id = "textArea"/>
-                        </label>
-                        <label> OR upload your recipe </label>
-                        <input type = "file" />
-                    <br/>
-                    <input onClick = {this.handleShare} value ="SHARE" id = "button"/>
-
-                        <label>
-                            <input type = "radio" name = "options" value = "Share your feedback" className = "form-check-input" />
-                            Share your feedback
-                        </label>
-                        <label>
-                            <textarea id ="textArea"/>
-                        </label>
-                    <input onClick = {this.handleSubmit} value = "SUBMIT" id = "button"/>
-                 </li>
-                 </ul>
-                 </div>
-                </form>
-                </html>
-            );
-        }
+        this.handleSubmitRecipe = this.handleSubmitRecipe.bind(this);
+        this.handleSubmitFeedback = this.handleSubmitFeedback.bind(this);
     }
-        
 
-    export default GetInTouchForm;
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmitRecipe(event) {
+        alert('Thank you ' + this.state.value+' for sharing your recipe with us. We will review and feature it on Foodwali ');
+        event.preventDefault();
+    }
+
+    handleSubmitFeedback(event) {
+        alert('Thank you ' + this.state.value+' for sharing your feedback with us.');
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div class="main">
+                <form>
+                    <label for="name">Your Name</label>
+                    <input type="text" id="name" name="name" placeholder="Your name.." value={this.state.value} onChange={this.handleChange}></input>
+
+                    <label for="email">Your email</label>
+                    <input type="text" id="email" name="email" placeholder="Your email.."></input>
+
+                    <input type="radio" id="submitrecipe" name="option" value="submitrecipe" checked></input>
+                    <label for="submitrecipe">Drop us a recipe</label><br></br>
+                    <textarea id="recipe" name="recipe" placeholder="Write something.."></textarea>
+                    <label> OR upload your recipe </label>
+                    <input type = "file" />
+                    <br></br>
+                    <br></br>
+                    <input onClick={this.handleSubmitRecipe} type="submit" value="Submit recipe"></input>
+                    
+                    <br></br>
+                    <br></br>
+
+                    <input type="radio" id="submitfeedback" name="option" value="submitfeedback"></input>
+                    <label for="submitfeedback">Share your feedback</label><br></br>
+                    <textarea id="feedback" name="feedback" placeholder="Write something.."></textarea>
+                    <input onClick={this.handleSubmitFeedback} type="submit" value="Share feedback"></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default GetInTouchForm;
